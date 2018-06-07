f1a5f1cf26c5d19d122f8779f12c06e0ca2bf85d
TylerHendrickson <tyler@labs.shiftgig.com>
2018-06-06 23:24:20 -0500

    Starting example code

--- handlers.py
+++ handlers.py
@@ -13,9 +13,6 @@ def put_product__http(event, context):
         'description': payload['description']
     }
 
-    dynamo_db = boto3.resource('dynamodb')
-    table = dynamo_db.Table(name=os.getenv('PRODUCT_TABLE_NAME'))
-    table.put_item(Item=product)
     print('Saved product {}'.format(product['name']))
 
     return {
@@ -24,53 +21,4 @@ def put_product__http(event, context):
         'headers': {
             'Content-Type': 'application/json'
         }
-    }
-
-
-def get_product__http(event, context):
-    product_name = unquote(event['pathParameters']['name'])
-
-    dynamo_db = boto3.resource('dynamodb')
-    table = dynamo_db.Table(name=os.getenv('PRODUCT_TABLE_NAME'))
-    result = table.get_item(Key={'name': product_name})
-
-    try:
-        product = result['Item']
-    except KeyError:
-        print('Could not find product {}'.format(product_name))
-        return {
-            'statusCode': '404',
-            'body': json.dumps({'error': 'Product not found'}),
-            'headers': {
-                'Content-Type': 'application/json'
-            }
-        }
-    print('Retrieved product {}'.format(product_name))
-
-    return {
-        'statusCode': '200',
-        'body': json.dumps(
-            {
-                'name': product['name'],
-                'price': product['price'],
-                'description': product['description']
-            }
-        ),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
-    }
-
-
-def delete_product__http(event, context):
-    product_name = unquote(event['pathParameters']['name'])
-
-    dynamo_db = boto3.resource('dynamodb')
-    table = dynamo_db.Table(name=os.getenv('PRODUCT_TABLE_NAME'))
-    table.delete_item(Key={'name': product_name})
-    print('Deleted product {}'.format(product_name))
-
-    return {
-        'statusCode': '204',
-        'body': ''
     }
